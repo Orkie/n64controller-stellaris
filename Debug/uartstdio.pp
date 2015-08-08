0c2396bd22d919fa1f9a371f19a33feec487216f
Adan Scotney <Adan Scotney@localhost>
2015-08-08 12:18:52 +0100

    Can now control LED with A button

---
+++ Debug/uartstdio.pp
@@ -0,0 +1,41 @@
+# FIXED
+
+uartstdio.obj: ../uartstdio.c
+uartstdio.obj: /opt/tiva/ccs/ccsv5/tools/compiler/arm_5.1.1/include/stdint.h
+uartstdio.obj: /opt/tiva/ccs/ccsv5/tools/compiler/arm_5.1.1/include/stdbool.h
+uartstdio.obj: /opt/tiva/ccs/ccsv5/tools/compiler/arm_5.1.1/include/yvals.h
+uartstdio.obj: /opt/tiva/ccs/ccsv5/tools/compiler/arm_5.1.1/include/stdarg.h
+uartstdio.obj: /opt/tiva/ccs/ccsv5/tools/compiler/arm_5.1.1/include/linkage.h
+uartstdio.obj: /opt/tiva/ccs/ccsv5/tools/compiler/arm_5.1.1/include/_lock.h
+uartstdio.obj: /opt/tiva/ccs/ccsv5/tools/compiler/arm_5.1.1/include/stdarg.h
+uartstdio.obj: /opt/tiva/tivaware/inc/hw_ints.h
+uartstdio.obj: /opt/tiva/tivaware/inc/hw_memmap.h
+uartstdio.obj: /opt/tiva/tivaware/inc/hw_types.h
+uartstdio.obj: /opt/tiva/tivaware/inc/hw_uart.h
+uartstdio.obj: /opt/tiva/tivaware/driverlib/debug.h
+uartstdio.obj: /opt/tiva/tivaware/driverlib/interrupt.h
+uartstdio.obj: /opt/tiva/tivaware/driverlib/rom.h
+uartstdio.obj: /opt/tiva/tivaware/driverlib/rom_map.h
+uartstdio.obj: /opt/tiva/tivaware/driverlib/sysctl.h
+uartstdio.obj: /opt/tiva/tivaware/driverlib/uart.h
+uartstdio.obj: ../uartstdio.h
+
+../uartstdio.c: 
+/opt/tiva/ccs/ccsv5/tools/compiler/arm_5.1.1/include/stdint.h: 
+/opt/tiva/ccs/ccsv5/tools/compiler/arm_5.1.1/include/stdbool.h: 
+/opt/tiva/ccs/ccsv5/tools/compiler/arm_5.1.1/include/yvals.h: 
+/opt/tiva/ccs/ccsv5/tools/compiler/arm_5.1.1/include/stdarg.h: 
+/opt/tiva/ccs/ccsv5/tools/compiler/arm_5.1.1/include/linkage.h: 
+/opt/tiva/ccs/ccsv5/tools/compiler/arm_5.1.1/include/_lock.h: 
+/opt/tiva/ccs/ccsv5/tools/compiler/arm_5.1.1/include/stdarg.h: 
+/opt/tiva/tivaware/inc/hw_ints.h: 
+/opt/tiva/tivaware/inc/hw_memmap.h: 
+/opt/tiva/tivaware/inc/hw_types.h: 
+/opt/tiva/tivaware/inc/hw_uart.h: 
+/opt/tiva/tivaware/driverlib/debug.h: 
+/opt/tiva/tivaware/driverlib/interrupt.h: 
+/opt/tiva/tivaware/driverlib/rom.h: 
+/opt/tiva/tivaware/driverlib/rom_map.h: 
+/opt/tiva/tivaware/driverlib/sysctl.h: 
+/opt/tiva/tivaware/driverlib/uart.h: 
+../uartstdio.h: 
