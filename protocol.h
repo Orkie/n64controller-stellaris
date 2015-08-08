4cbd3b47d9e91a2a3b572e9e69eb3310368a1abb
Adan <adan.scotney@gmail.com>
2015-08-08 12:25:55 +0100

    USB reenabled

--- protocol.h
+++ protocol.h
@@ -12,7 +12,5 @@ void GCN64InitializeProtocol(void);
 
 void n64Transmit(uint8_t bufer[], int length);
 int n64Receive(uint8_t buffer[]);
-unsigned long GCN64Send(unsigned long ulPort, unsigned char ucPin, unsigned char *pucBuffer, unsigned long ulToSend);
-unsigned long GCN64Receive(unsigned long ulPort, unsigned char ucPin, unsigned char *pucBuffer);
 
 #endif /* PROTOCOL_H_ */
